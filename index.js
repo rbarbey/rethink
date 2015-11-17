1224411ec553cdd8c749d55966b029debe80f183
Robert Barbey <robert.barbey@gmail.com>
2015-11-17 23:44:10 +0100

    Added serving of index.html file

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ var app = module.exports = express();
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
+function serveIndex(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+}
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-var todos = require('./routes/todos');
-
-app.use('/todos', todos);
+app.use('/', serveIndex);
+app.use('/todos', require('./routes/todos'));
 
 app.on('start', function () {
   console.log('Application ready to serve requests');
